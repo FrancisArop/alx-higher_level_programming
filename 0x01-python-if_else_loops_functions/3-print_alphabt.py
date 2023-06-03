a901492924de475a7e8917783e6133033242481d
FrancisArop <francismagood@gmail.com>
2023-06-03 19:00:58 +0000

    3-print_alphabt.py

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# My name is Francis Arop - 3-June-23
+for letter in range(97, 123):
+        if chr(letter) != 'q' and chr(letter) != 'e':
+                    print("{}".format(chr(letter)), end="")
