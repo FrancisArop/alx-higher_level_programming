8097dfd135f4dfed5d5206ea5775e2c685cff593
FrancisArop <francismagood@gmail.com>
2023-06-04 18:14:27 +0000

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Yak Arop Magud 
+# Author - Yak Arop Magud - 5-June-23
 """Print the numbers from 1 to 100 separated by a space.
   For multiples of three, print Fizz instead of the number
   For multiples of five, print Buzz instead of the number.
