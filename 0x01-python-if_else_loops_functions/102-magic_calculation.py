1d720645704bc1c7e9b88f15c45e8704b16a75cc
FrancisArop <francismagood@gmail.com>
2023-06-05 07:57:45 +0000

    102-magic_calculation.py

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Written by Francis Arop - 5-June-23
+
+def magic_calculation (a, b, c):
+    """Match bytecode proveded by Holbberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a,b)
+    return (a*b - c)
