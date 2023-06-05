f0bb22ec2d583014cff42be88238d3350bb94d20
FrancisArop <francismagood@gmail.com>
2023-06-05 08:14:07 +0000

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-        """Print the sum of 1 and 2."""
-            from add_0 import add
+# Author - Yak Arop
 
-                a = 1
-                    b = 2
-                        print("{} + {} = {}".format(a, b, add(a, b)))
+if _name_ == "_main_":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
 
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, aa(a, b)))
